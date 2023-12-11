cef17b04f88f9c6fdbdba28d8c08e1dbbb8e4f81
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-11 15:00:53 +0330

    ADD db_setup

---
+++ dp/db_setup.py
@@ -0,0 +1,19 @@
+from os import getenv
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{getenv('POSTGRES_USER')}:{getenv('POSTGRES_PASSWORD')}@{getenv('POSTGRES_HOST')}:{getenv('POSTGRES_PORT')}/{getenv('POSTGRES_DATABASE')}"
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
+
+
+# DB Utilities
+async def async_get_db():
+    async with SessionLocal() as db:
+        yield db
+        await db.commit()
