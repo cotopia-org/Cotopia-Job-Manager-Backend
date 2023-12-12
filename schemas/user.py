fdaf7c9ba574bd81435d100a9c909eaae63f6516
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-13 00:53:06 +0330

    CHANGE user schema

--- schemas/user.py
+++ schemas/user.py
@@ -5,16 +5,22 @@ from pydantic import BaseModel
 
 class UserBase(BaseModel):
     email: str
-    role: int
-
 
 class UserCreate(UserBase):
-    ...
+    password: str
 
 
-class User(UserBase):
+class UserUpdate(UserCreate):
+    password: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    discord_user_id: int | None = None
+
+
+class User(UserUpdate):
     id: int
     is_active: bool
+    role: int
     created_at: datetime
     updated_at: datetime
 
