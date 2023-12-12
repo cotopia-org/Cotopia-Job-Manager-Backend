2478525ec2fe68fb312a8eb8a5d8b5226cadef01
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-13 00:59:48 +0330

    CHANGE user CRUD

--- api/utils/users.py
+++ api/utils/users.py
@@ -1,12 +1,14 @@
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
 from db.models.user import User
 from schemas.user import UserCreate
 
-# async def get_user(db: AsyncSession, user_id: int):
-#     query = select(User).where(User.id == user_id)
-#     result = await db.execute(query)
-#     return result.scalar_one_or_none()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
@@ -17,8 +19,15 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(User).offset(skip).limit(limit).all()
 
 
+def get_password_hash(password):
+    if password is None:
+        return None
+    return pwd_context.hash(password)
+
+
 def create_user(db: Session, user: UserCreate):
-    db_user = User(email=user.email, role=user.role)
+    hashed_password = get_password_hash(user.password)
+    db_user = User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
