ade94eaa25802e0021b5a0b105c9679e1c4f2a11
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-13 15:27:29 +0330

    CHANGE make user utils more readable

--- api/utils/users.py
+++ api/utils/users.py
@@ -1,22 +1,22 @@
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
-from db.models.user import User
+from db.models.user import User as UserModel
 from schemas.user import UserCreate, UserUpdate
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+    return db.query(UserModel).filter(UserModel.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+    return db.query(UserModel).filter(UserModel.email == email).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(User).offset(skip).limit(limit).all()
+    return db.query(UserModel).offset(skip).limit(limit).all()
 
 
 def get_password_hash(password):
@@ -27,7 +27,7 @@ def get_password_hash(password):
 
 def create_user(db: Session, user: UserCreate):
     hashed_password = get_password_hash(user.password)
-    db_user = User(email=user.email, hashed_password=hashed_password)
+    db_user = UserModel(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
