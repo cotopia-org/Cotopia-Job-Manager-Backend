eac859e77bfa56eafe91a4571fe61220b6d7dc68
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-13 14:04:49 +0330

    ADD comment schemas

---
+++ schemas/comment.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class CommentBase(BaseModel):
+    author_id: int
+    job_id: int
+    body: str
+
+class CommentCreate(CommentBase):
+    ...
+
+
+class CommentUpdate(CommentCreate):
+    body: str
+    is_archived: bool | None = None
+
+
+class Comment(CommentUpdate):
+    id: int
+    is_archived: bool
+    created_at: datetime
+    updated_at: datetime
+    author_id: int
+    job_id: int
+    body: str
