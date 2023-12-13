47f2aa870f60688188c1dc074de041abad284a4b
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-13 22:03:11 +0330

    CHANGE alembic setup

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 from db.db_setup import Base
-from db.models import user   # noqa: F401
+from db.models import user, job, comment  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
