b3d7395e4329e38f156ce7317d36b02313686ec3
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-13 12:13:57 +0330

    ADD submitted_jobs and accepted_jobs to user model

--- db/models/user.py
+++ db/models/user.py
@@ -3,7 +3,6 @@ import enum as pyEnum
 from sqlalchemy import BigInteger, Boolean, Column, Enum, Integer, String
 from sqlalchemy.orm import relationship
 
-
 from ..db_setup import Base
 from .mixins import Timestamp
 
@@ -23,4 +22,7 @@ class User(Timestamp, Base):
     first_name = Column(String(50), nullable=True)
     last_name = Column(String(50), nullable=True)
     discord_user_id = Column(BigInteger, nullable=True)
-    jobs = relationship("Job", secondary="users_jobs", back_populates="acceptor_users")
+    submitted_jobs = relationship("Job", back_populates="creator")
+    accepted_jobs = relationship(
+        "Job", secondary="users_jobs", back_populates="acceptor_users"
+    )
