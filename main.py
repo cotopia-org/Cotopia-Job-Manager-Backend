935d44e9657e3b4ace89c8a2efaf334365f6dc04
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-13 18:51:16 +0330

    ADD jobs routers to app

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from api import users, comments
+from api import comments, jobs, users
 from db.db_setup import engine
-from db.models import user, comment
+from db.models import comment, job, user
 
 user.Base.metadata.create_all(bind=engine)
 comment.Base.metadata.create_all(bind=engine)
+job.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Job Manager API",
@@ -14,8 +15,9 @@ app = FastAPI(
     contact={
         "name": "Ali Kharrati",
         "email": "ali.kharrati@gmail.com",
-    }
+    },
 )
 
 app.include_router(users.router)
 app.include_router(comments.router)
+app.include_router(jobs.router)
